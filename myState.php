abf82fe1bf5a0526645387b5e86b5f60e69e75d6
DenyRamdhany <deny_ramdhany@apps.ipb.ac.id>
2016-12-24 23:25:40 +0700

    Memperpendek MyState.php

--- myState.php
+++ myState.php
@@ -2,9 +2,6 @@
 	include("config.php");
 	extract($_GET);
 	
-	$qry=mysql_query("SELECT * FROM billing where station LIKE $dev AND port LIKE $port");
-	
-	while($data=mysql_fetch_object($qry))
-	{ echo "{".$data->status."}";
-	}
+	$data=mysql_fetch_object(mysql_query("SELECT * FROM billing where station LIKE $dev AND port LIKE $port"));
+	echo "{".$data->status."}";
 ?>
