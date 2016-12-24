75d32b3b5f6f33db7c691a868406943e57a16a11
DenyRamdhany <deny_ramdhany@apps.ipb.ac.id>
2016-12-24 23:53:52 +0700

    Database Conn Edit
    change user & SQL file added

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	$host = "localhost";
-	$user = "root";
-	$pass = "dancenanta";
+	$user = "anyone";
+	$pass = "anyone";
 	$db = "gammu";
 	
 	mysql_connect($host, $user, $pass) or die (mysql_error());
